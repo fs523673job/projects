22422d3cc1ee1d4ce026391e141c6ed8756fdd1f
flsantos <flsantos@apdata.com.br>
2023-07-05 14:55:33 -0300

    Commit realizado em -7/-02 14:55:33

---
+++ allupdate.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+
+:: Configuração do repositório
+set REPO_PATH=C:\github\fs523673job\projects
+
+:: Atualizando o repositório
+cd /d %REPO_PATH%
+git pull
+
+:: Executando o script update.bat em cmdSQL
+cd /d %REPO_PATH%\cmdSQL
+call update.bat
+
+:: Executando o script update.bat em cmdGIT
+cd /d %REPO_PATH%\cmdGIT
+call update.bat
+
+:: Adicionando todas as mudanças no repositório
+cd /d %REPO_PATH%
+git add --all
+
+:: Criando uma mensagem de commit com a data e hora atuais
+set DATE_TIME=%date:~10,4%-%date:~4,2%-%date:~7,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
+set COMMIT_MSG=Commit realizado em %DATE_TIME%
+
+:: Realizando o commit
+git commit -m "%COMMIT_MSG%"
+
+:: Empurrando as mudanças para o servidor
+git push
+
+endlocal
---
+++ cmdGIT/update.bat
@@ -0,0 +1,3 @@
+cd "C:\github\fs523673job\projects\cmdGIT"
+copy "C:\Program Files\Git\usr\bin\git-fs*" "C:\github\fs523673job\projects\cmdGIT"
+
