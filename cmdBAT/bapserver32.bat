20390d4fb37d86423019039bccb30ee4b5c3feb2
flsantos <flsantos@apdata.com.br>
2023-07-28 19:00:38 -0300

    Commit realizado em 2023-07-28 19:00:38

---
+++ cmdBAT/bapserver32.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+:: Defina aqui os valores padrão para cada argumento
+set "vrsDelphi=Alexandria"
+set "typeBuild=Debug"
+set "dirBase=ApData_X64"
+set "dirApp=ApServer"
+set "arquitetura=Win32"
+set "appName=ApServer"
+set "addEureka=0"
+
+set "PRN_DEFAULT=localhost /start"
+
+:: Chama o primeiro script com os valores padrão
+call "C:\github\fs523673job\projects\cmdBAT\build.bat" %vrsDelphi% %typeBuild% %dirBase% %dirApp% %arquitetura% %appName% %addEureka%  
+
+endlocal
