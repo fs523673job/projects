2a8913e5087bc344e47de0d0bb8217dbe5c6ca65
flsantos <flsantos@apdata.com.br>
2023-08-09 17:12:12 -0300

    Commit realizado em 2023-08-09 17:12:12

--- cmdGIT/update.bat
+++ cmdGIT/update.bat
@@ -1,4 +1,3 @@
 cd "C:\github\fs523673job\projects\cmdGIT"
-copy "C:\Program Files\Git\usr\bin\git-fs*" "C:\github\fs523673job\projects\cmdGIT"
-copy "C:\github\fs523673job\projects\cmdGIT\git-fs*" "\\srvdevel\Shared\Fernando\Gits_Scripts"
+copy "C:\github\fs523673job\projects\cmdGIT\git-fs*" "C:\Program Files\Git\usr\bin\git-fs*" 
 
