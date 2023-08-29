d7bed586e6f0a52135f9f94713f0f3b6451de6c5
flsantos <flsantos@apdata.com.br>
2023-08-29 15:14:45 -0300

    Commit realizado em 2023-08-29 15:14:45

--- cmdPython/createFeatureFromBranch.py
+++ cmdPython/createFeatureFromBranch.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 
 
-def insert_into_firebird(input_name, input_sha, branch_name, regex_search):
+def insert_into_firebird(input_name, input_sha, branch_name, regex_search, feature_desc, feature_type):
     # Configurações de conexão com o banco Firebird
     DB_HOST = 'localhost'
     DB_NAME = r'C:\github\bases\firebird\TESTDB_F30.FDB'
@@ -20,14 +20,18 @@ def insert_into_firebird(input_name, input_sha, branch_name, regex_search):
 
     # SQL para inserir os dados
     sql = """
-    INSERT INTO FEATURE_CRIADAS (NAME_FEATURE, NAME_BRANCH, SHA_ORIGEM, REGEX_SEARCH) 
-    VALUES (?, ?, ?, ?)
+    INSERT INTO FEATURE_CRIADAS (NAME_FEATURE, NAME_BRANCH, SHA_ORIGEM, REGEX_SEARCH, TYPE_FEATURE, DESC_PT) 
+    VALUES (?, ?, ?, ?, ?, ?)
     """
 
     # Executa a SQL
-    cur.execute(sql, (input_name, input_sha, branch_name, regex_search))
+    cur.execute(sql, (input_name, input_sha, branch_name, regex_search, feature_type, feature_desc))
     con.commit()
     con.close()
+	
+
+def get_feature_type(feature_name: str) -> str:
+    return feature_name.split('_')[-1]
 
 
 def main():
@@ -37,7 +41,9 @@ def main():
     # Chama o script bash e obtém a saída
     branch_name = input("Digite o nome da branch: ")
     feature_name = input("Digite o nome da feature: ")
+    feature_desc = input("Digite a descricao do chamado: ")
     regex_search = input("Digite o regex de busca: ")
+    feature_type = get_feature_type(feature_name)  # Corrigido a indentação aqui
 
     args = ['C:/Program Files/Git/bin/git.exe',
             'fscreatefeaturefrombranch', branch_name, feature_name]
@@ -55,7 +61,7 @@ def main():
 
     if 'NAME_FEATURE' in data and 'NAME_BRANCH' in data and 'SHA_ORIGEM' in data:
         insert_into_firebird(
-            data['NAME_FEATURE'], data['NAME_BRANCH'], data['SHA_ORIGEM'], regex_search)
+            data['NAME_FEATURE'], data['NAME_BRANCH'], data['SHA_ORIGEM'], regex_search, feature_desc, feature_type)
     else:
         print("Erro: Não foi possível obter todas as informações necessárias da saída do script Bash.")
 
