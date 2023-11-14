8abe22aced77026cca8dedb2f6e4bca4d17e3a6a
flsantos <flsantos@apdata.com.br>
2023-11-14 11:59:30 -0300

    Commit realizado em 2023-11-14 11:59:30

--- cmdBAT/apserver64.bat
+++ cmdBAT/apserver64.bat
@@ -6,7 +6,7 @@ set "VRS_DEFAULT=Apdata_X64"
 set "APP_DEFAULT=ApServer"
 set "ARQ_DEFAULT=Win64"
 set "BLD_DEFAULT=Debug"
-set "PRN_DEFAULT=localhost /start"
+set "PRN_DEFAULT=localhost /start /enablelog /integration_timeout 130"
 
 :: Chama o primeiro script com os valores padrão
 call "C:\github\fs523673job\projects\cmdBAT\run.bat" %VRS_DEFAULT% %APP_DEFAULT% %APP_DEFAULT% %ARQ_DEFAULT% %BLD_DEFAULT% "%PRN_DEFAULT%"
--- cmdBAT/spawndebug2_64.bat
+++ cmdBAT/spawndebug2_64.bat
@@ -4,9 +4,9 @@ setlocal
 :: Defina aqui os valores padrão para cada argumento
 set "VRS_DEFAULT=Apdata_X64"
 set "APP_DEFAULT=ApServer"
-set "ARQ_DEFAULT=Win32"
+set "ARQ_DEFAULT=Win64"
 set "BLD_DEFAULT=Debug"
-set "PRN_DEFAULT=localhost /start /enablelog /integration_timeout 130"
+set "PRN_DEFAULT=localhost /start /spawndebug 7002"
 
 :: Chama o primeiro script com os valores padrão
 call "C:\github\fs523673job\projects\cmdBAT\run.bat" %VRS_DEFAULT% %APP_DEFAULT% %APP_DEFAULT% %ARQ_DEFAULT% %BLD_DEFAULT% "%PRN_DEFAULT%"
