aa1d3e3462e4901cbf51abf911351b9c6ad80697
flsantos <flsantos@apdata.com.br>
2023-12-21 14:55:25 -0300

    Commit realizado em 2023-12-21 14:55:24

---
+++ cmdBAT/papscripter.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+::Abrir o delphi com o projeto do ApScripter
+
+:: Defina aqui os valores padrão para cada argumento
+set "VRS_DEFAULT=Apdata_X64"
+set "APP_DEFAULT=ApScripter"
+set "NAME_DEFAULT=ApScripter"
+
+:: Check if a version argument was passed in
+if "%~1"=="" (
+    set "DELPHIVERSION=22.0"
+) else (
+    if /I "%~1"=="DelphiTokio" (
+        set "DELPHIVERSION=19.0"
+    ) else if /I "%~1"=="DelphiAlexandria" (
+        set "DELPHIVERSION=22.0"
+    )
+)
+
+:: Chama o primeiro script com os valores padrão
+call "C:\github\fs523673job\projects\cmdBAT\prun.bat" %VRS_DEFAULT% %APP_DEFAULT% %NAME_DEFAULT% %DELPHIVERSION%
+
+endlocal
---
+++ cmdSQL/dbeaver.sql
@@ -0,0 +1,11 @@
+SELECT ID, PT_NOME, DESCRICAO, PALAVRAS_CHAVE, DATA_INICIO, DATA_FINAL FROM CHAMADOS c ORDER BY 1 DESC;	
+
+SELECT * FROM DESVIOS d ORDER BY 1 DESC;
+
+SELECT * FROM FEATURE_CRIADAS fc ORDER BY 1 DESC;
+
+/***************************************************************************************************************************/
+
+ 
+
+
