6d3ab3a32ad71805d99eb67d4b49df0b4c1d2310
flsantos <flsantos@apdata.com.br>
2024-04-12 17:08:15 -0300

    Commit realizado em 2024-04-12 17:08:15

--- testesDelphi/AnalyseSQL/unMain.pas
+++ testesDelphi/AnalyseSQL/unMain.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SynEditHighlighter, SynHighlighterSQL, SynEdit, Vcl.Buttons, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, SynEditHighlighter, SynHighlighterSQL, SynEdit, Vcl.Buttons, Vcl.ExtCtrls,
+  StrUtils;
 
 type
   TForm1 = class(TForm)
@@ -34,41 +35,28 @@ uses
 
 procedure TForm1.SpeedButton1Click(Sender: TObject);
 var
-  SQL: String;
+  SQLSanitized: String;
   Conditions: TStringList;
   Condition: String;
 begin
   SynEdit2.Lines.Clear;
 
-  SQL := LinearizeSQL(SynEdit1.Text);
-//  Conditions := ExtrairCondicoesWhereComOr(SQL);
-//  try
-//    for Condition in Conditions do
-//    begin
-//      SynEdit2.Lines.Add(Condition);
-//      SynEdit2.Lines.Add(StringOfChar('*', 100));
-//      SynEdit2.Lines.Add(ExtrairOperadoresExternos(Condition));
-//      SynEdit2.Lines.Add(StringOfChar('*', 100));
-//    end;
-//  finally
-//    Conditions.Free;
-//  end;
+  SQLSanitized := LinearizeSQL(SynEdit1.Text);
 
-  Conditions := RemoveAutoEmployeeFilters(SQL);
+  Conditions := RemoveAutoEmployeeFilters(SQLSanitized);
   try
     for Condition in Conditions do
     begin
       SynEdit2.Lines.Add(Condition);
       SynEdit2.Lines.Add(StringOfChar('*', 100));
-      //SynEdit2.Lines.Add(ExtrairOperadoresExternos(Condition));
-      //SynEdit2.Lines.Add(StringOfChar('*', 100));
     end;
   finally
     Conditions.Free;
   end;
 
+  SQLSanitized := LinearizeSQL(SynEdit1.Text);
 
-
+  SynEdit2.Lines.Add('Existe Or sem Fechamento: ' + IfThen(BoolToStr(AnaliseSQLClausesOR(SQLSanitized), True) = 'True', 'Sim', 'Não'));
 end;
 
 procedure TForm1.SpeedButton2Click(Sender: TObject);
--- testesDelphi/AnalyseSQL/unTestSQL.pas
+++ testesDelphi/AnalyseSQL/unTestSQL.pas
@@ -15,6 +15,7 @@ function ExtrairCondicoesWhereComOr(SQL: string): TStringList;
 function ExtrairOperadoresExternos(const Expr: string): string;
 function RemoveUmTrechoAutoEmployeeFilter(var SQLText: string): string;
 function RemoveAutoEmployeeFilters(var SQLText: string): TStringList;
+function AnaliseSQLClausesOR(const ASQL: string): Boolean;
 
 
 implementation
@@ -25,7 +26,7 @@ var
   prevCharIsSpace: Boolean;
   auxSQL: String;
 begin
-  auxSQL := StringReplace(ASQL.ToLower,   #$D#$A, ' ', [rfReplaceAll]);
+  auxSQL := StringReplace(ASQL.ToUpper,   #$D#$A, ' ', [rfReplaceAll]);
 
   prevCharIsSpace := False;
 
@@ -71,14 +72,14 @@ end;
 function ContemOrValido(const Condicao: string): Boolean;
 begin
   // Verifica se a condição contém "or" com várias possíveis formatações
-  Result := Pos(' or ', LowerCase(Condicao)) > 0;
-  Result := Result or (Pos(')or(', LowerCase(Condicao)) > 0);
-  Result := Result or (Pos(')or', LowerCase(Condicao)) > 0);
-  Result := Result or (Pos('or(', LowerCase(Condicao)) > 0);
-  Result := Result or (Pos(' or', LowerCase(Condicao)) > 0);
-  Result := Result or (Pos('or ', LowerCase(Condicao)) > 0);
-  Result := Result or (Pos(')or ', LowerCase(Condicao)) > 0);
-  Result := Result or (Pos(' or)', LowerCase(Condicao)) > 0);
+  Result := Pos(' OR ', Condicao.ToUpper) > 0;
+  Result := Result or (Pos(')OR(', Condicao.ToUpper) > 0);
+  Result := Result or (Pos(')OR', Condicao.ToUpper) > 0);
+  Result := Result or (Pos('OR(', Condicao.ToUpper) > 0);
+  Result := Result or (Pos(' OR', Condicao.ToUpper) > 0);
+  Result := Result or (Pos('OR ', Condicao.ToUpper) > 0);
+  Result := Result or (Pos(')OR ', Condicao.ToUpper) > 0);
+  Result := Result or (Pos(' OR)', Condicao.ToUpper) > 0);
 end;
 
 function ExtrairCondicoesWhereComOr(SQL: string): TStringList;
@@ -89,21 +90,21 @@ begin
   Result := TStringList.Create; // Cria a lista para armazenar as condições
   SQLLower := LowerCase(SQL); // Converte o SQL para minúsculas para facilitar a busca
   // Loop para encontrar todas as ocorrências do comentário
-  PosicaoComentario := Pos('/*autoemployeefilter', SQLLower);
+  PosicaoComentario := Pos('/*AUTOEMPLOYEEFILTER', SQLLower);
   while PosicaoComentario > 0 do
   begin
     // Encontra a posição do próximo comentário para limitar a busca do WHERE ao bloco atual
-    PosicaoProximoComentario := PosEx('/*autoemployeefilter', SQLLower, PosicaoComentario + Length('/*autoemployeefilter'));
+    PosicaoProximoComentario := PosEx('/*AUTOEMPLOYEEFILTER', SQLLower, PosicaoComentario + Length('/*AUTOEMPLOYEEFILTER'));
     if PosicaoProximoComentario = 0 then
       PosicaoProximoComentario := MaxInt;
     // Encontra a última posição do WHERE antes do comentário, limitando a busca ao bloco atual
-    PosicaoInicio := UltimaPosicaoSubString('where', Copy(SQLLower, 1, PosicaoComentario));
+    PosicaoInicio := UltimaPosicaoSubString('WHERE', Copy(SQLLower, 1, PosicaoComentario));
     if (PosicaoInicio > 0) and (PosicaoInicio < PosicaoProximoComentario) then
     begin
-      Inc(PosicaoInicio, Length('where'));
-      PosicaoFim := PosEx('group by', SQLLower, PosicaoInicio);
+      Inc(PosicaoInicio, Length('WHERE'));
+      PosicaoFim := PosEx('GROUP by', SQLLower, PosicaoInicio);
       if (PosicaoFim = 0) or (PosicaoFim > PosicaoProximoComentario) then
-        PosicaoFim := PosEx('order by', SQLLower, PosicaoInicio);
+        PosicaoFim := PosEx('ORDER by', SQLLower, PosicaoInicio);
       if (PosicaoFim = 0) or (PosicaoFim > PosicaoProximoComentario) then
         PosicaoFim := PosicaoProximoComentario;
       if PosicaoFim > PosicaoInicio then
@@ -111,7 +112,7 @@ begin
       else
         TempResult := Trim(Copy(SQL, PosicaoInicio, PosicaoComentario - PosicaoInicio));
       // Remove o comentário /*AutoEmployeeFilter*/ e qualquer texto após ele
-      PosicaoFimComentario := Pos('/*autoemployeefilter', LowerCase(TempResult));
+      PosicaoFimComentario := Pos('/*AUTOEMPLOYEEFILTER', LowerCase(TempResult));
       if PosicaoFimComentario > 0 then
         CondicaoSemComentario := Copy(TempResult, 1, PosicaoFimComentario - 1)
       else
@@ -126,72 +127,72 @@ begin
     // Prepara para buscar a próxima ocorrência do comentário, se houver
     if PosicaoProximoComentario <> MaxInt then
       //PosicaoComentario := PosEx('/*autoemployeefilter', SQLLower, PosicaoProximoComentario)
-      PosicaoComentario := Pos('/*autoemployeefilter', SQLLower)
+      PosicaoComentario := Pos('/*AUTOEMPLOYEEFILTER', SQLLower)
     else
       Break; // Sai do loop se não houver mais comentários
   end;
+end;
+
+function ExtrairOperadoresExternos(const Expr: string): string;
+var
+  i, ParentesesNivel: Integer;
+  TempResult: string;
+  ForaParenteses: string;
+begin
+  TempResult := ''; // Inicializa o resultado temporário
+  ParentesesNivel := 0; // Inicializa o contador de nível de parênteses
+  ForaParenteses := '';
+
+  // Percorre cada caracter da string
+  for i := 1 to Length(Expr) do
+  begin
+    if Expr[i] = '(' then
+      Inc(ParentesesNivel)
+    else if Expr[i] = ')' then
+      Dec(ParentesesNivel)
+    else if ParentesesNivel = 0 then
+      ForaParenteses := ForaParenteses + Expr[i];
+  end;
+
+  // Aplica a lógica de ContemOrValido para o texto fora dos parênteses
+  if ContemOrValido(ForaParenteses) then
+    Result := 'OR'
+  else
+    Result := ''; // Não encontrou "OR" válido fora dos parênteses
 end;
 
 //function ExtrairOperadoresExternos(const Expr: string): string;
 //var
 //  i, ParentesesNivel: Integer;
 //  TempResult: string;
-//  ForaParenteses: string;
 //begin
 //  TempResult := ''; // Inicializa o resultado temporário
 //  ParentesesNivel := 0; // Inicializa o contador de nível de parênteses
-//  ForaParenteses := '';
-//
 //  // Percorre cada caracter da string
 //  for i := 1 to Length(Expr) do
 //  begin
+//    // Verifica se o caracter atual é um parêntese de abertura
 //    if Expr[i] = '(' then
-//      Inc(ParentesesNivel)
+//      Inc(ParentesesNivel) // Incrementa o nível de parênteses
 //    else if Expr[i] = ')' then
-//      Dec(ParentesesNivel)
+//      Dec(ParentesesNivel) // Decrementa o nível de parênteses
 //    else if ParentesesNivel = 0 then
-//      ForaParenteses := ForaParenteses + Expr[i];
+//      // Se não estiver dentro de parênteses, adiciona o caracter ao resultado temporário
+//      TempResult := TempResult + Expr[i];
 //  end;
-//
-//  // Aplica a lógica de ContemOrValido para o texto fora dos parênteses
-//  if ContemOrValido(ForaParenteses) then
+//  // Remove espaços em branco extras do resultado temporário
+//  Result := Trim(TempResult);
+//  // Verifica se o resultado contém apenas operadores lógicos externos
+//  // Neste exemplo, simplificamos verificando apenas a presença do operador "OR" fora dos parênteses
+//  // Esta verificação pode ser expandida para outros operadores ou lógica conforme necessário
+//  if (Pos('OR', UpperCase(Result)) > 0) and (Pos('AND', UpperCase(Result)) = 0) then
 //    Result := 'OR'
+//  else if (Pos('AND', UpperCase(Result)) > 0) and (Pos('OR', UpperCase(Result)) = 0) then
+//    Result := 'AND'
 //  else
-//    Result := ''; // Não encontrou "OR" válido fora dos parênteses
+//    // Se a string resultante não corresponder a um único operador lógico externo, limpa o resultado
+//    Result := '';
 //end;
-
-function ExtrairOperadoresExternos(const Expr: string): string;
-var
-  i, ParentesesNivel: Integer;
-  TempResult: string;
-begin
-  TempResult := ''; // Inicializa o resultado temporário
-  ParentesesNivel := 0; // Inicializa o contador de nível de parênteses
-  // Percorre cada caracter da string
-  for i := 1 to Length(Expr) do
-  begin
-    // Verifica se o caracter atual é um parêntese de abertura
-    if Expr[i] = '(' then
-      Inc(ParentesesNivel) // Incrementa o nível de parênteses
-    else if Expr[i] = ')' then
-      Dec(ParentesesNivel) // Decrementa o nível de parênteses
-    else if ParentesesNivel = 0 then
-      // Se não estiver dentro de parênteses, adiciona o caracter ao resultado temporário
-      TempResult := TempResult + Expr[i];
-  end;
-  // Remove espaços em branco extras do resultado temporário
-  Result := Trim(TempResult);
-  // Verifica se o resultado contém apenas operadores lógicos externos
-  // Neste exemplo, simplificamos verificando apenas a presença do operador "OR" fora dos parênteses
-  // Esta verificação pode ser expandida para outros operadores ou lógica conforme necessário
-  if (Pos('OR', UpperCase(Result)) > 0) and (Pos('AND', UpperCase(Result)) = 0) then
-    Result := 'OR'
-  else if (Pos('AND', UpperCase(Result)) > 0) and (Pos('OR', UpperCase(Result)) = 0) then
-    Result := 'AND'
-  else
-    // Se a string resultante não corresponder a um único operador lógico externo, limpa o resultado
-    Result := '';
-end;
 
 function RemoveUmTrechoAutoEmployeeFilter(var SQLText: string): string;
 var
@@ -199,24 +200,20 @@ var
 begin
   Result := '';
   // Encontra o fim do comentário
-  CommentStart := Pos('/*autoemployeefilter=', SQLText);
+  CommentStart := Pos('/*AUTOEMPLOYEEFILTER=', SQLText);
   if CommentStart = 0 then Exit;
-
   EndPos := PosEx('*/', SQLText, CommentStart) + 2; // Inclui o fim do comentário
   if EndPos = 2 then Exit; // Não encontrou o fim do comentário
-
   // Busca para trás a partir do início do comentário para encontrar o WHERE mais próximo
   WherePos := CommentStart;
   repeat
     Dec(WherePos);
-    if Copy(SQLText, WherePos, 5).ToLower = 'where' then Break;
+    if Copy(SQLText, WherePos, 5).ToUpper = 'WHERE' then Break;
+    if Copy(SQLText, WherePos, 6).ToUpper = 'SELECT' then Break;
   until WherePos <= 1;
-
   if WherePos < 1 then Exit; // Não encontrou WHERE antes do comentário
-
   // Extrai o trecho
   Result := Copy(SQLText, WherePos, EndPos - WherePos);
-
   // Remove o trecho do SQL original
   Delete(SQLText, WherePos, EndPos - WherePos);
 end;
@@ -228,9 +225,131 @@ begin
   Result := TStringList.Create;
   repeat
     RemovedTrecho := RemoveUmTrechoAutoEmployeeFilter(SQLText);
-    if RemovedTrecho <> '' then
+    if not RemovedTrecho.IsEmpty then
       Result.Add(ExtrairOperadoresExternos(RemovedTrecho));
-  until RemovedTrecho = '';
+  until RemovedTrecho.IsEmpty;
+end;
+
+function AnaliseSQLClausesOR(const ASQL: string): Boolean;
+
+  function LastPosSubString(const SubStr, InStr: string): Integer;
+  var
+    LastPos, TempPos: Integer;
+  begin
+    Result := 0;
+    LastPos := Pos(SubStr, InStr);
+    while LastPos > 0 do
+    begin
+      TempPos := PosEx(SubStr, InStr, LastPos + 1);
+      if TempPos = 0 then
+        Break
+      else
+        LastPos := TempPos;
+    end;
+    if LastPos > 0 then
+      Result := LastPos;
+  end;
+
+  function ContainsValidOR(const Condition: String): Boolean;
+  begin
+    Result := Pos(' OR ', Condition.ToUpper) > 0;
+    Result := Result or (Pos(')OR(', Condition.ToUpper) > 0);
+    Result := Result or (Pos(')OR', Condition.ToUpper) > 0);
+    Result := Result or (Pos('OR(', Condition.ToUpper) > 0);
+    Result := Result or (Pos(' OR', Condition.ToUpper) > 0);
+    Result := Result or (Pos('OR ', Condition.ToUpper) > 0);
+    Result := Result or (Pos(')OR ', Condition.ToUpper) > 0);
+    Result := Result or (Pos(' OR)', Condition.ToUpper) > 0);
+  end;
+
+  function RemoveSnippetAutoEmployeeFilter(var SQLText: string): string;
+  var
+    StartPos, EndPos, WherePos, CommentStart: Integer;
+  begin
+    CommentStart := Pos('/*AUTOEMPLOYEEFILTER=', SQLText);
+
+    if CommentStart = 0 then
+      Exit('');
+
+    EndPos := PosEx('*/', SQLText, CommentStart) + 2;
+
+    if EndPos = 2 then
+      Exit('');
+
+    WherePos := CommentStart;
+    repeat
+      Dec(WherePos);
+      if Copy(SQLText, WherePos, 5).ToUpper = 'WHERE' then
+        Break;
+      if Copy(SQLText, WherePos, 6).ToUpper = 'SELECT' then
+        Break;
+    until WherePos <= 1;
+
+    if WherePos < 1 then
+      Exit('');
+
+    Result := Copy(SQLText, WherePos, EndPos - WherePos);
+
+    Delete(SQLText, WherePos, EndPos - WherePos);
+  end;
+
+  function ExtractWhereConditionsWithOR(const AASQL: string): TStringList;
+  var
+    SnippetSQL: string;
+    AlteredSQL: String;
+  begin
+    AlteredSQL := AASQL;
+
+    Result := TStringList.Create;
+    repeat
+      SnippetSQL := RemoveSnippetAutoEmployeeFilter(AlteredSQL);
+      if not SnippetSQL.IsEmpty then
+        Result.Add(SnippetSQL);
+    until SnippetSQL.IsEmpty;
+  end;
+
+  function ExtractInternalOperators(const Expr: string): string;
+  var
+    c, LevelPrentheses: Integer;
+    TempResult: string;
+  begin
+    TempResult := '';
+    LevelPrentheses := 0;
+
+    for c := 1 to Length(Expr) do
+    begin
+      if Expr[c] = '(' then
+        Inc(LevelPrentheses)
+      else if Expr[c] = ')' then
+        Dec(LevelPrentheses)
+      else if LevelPrentheses = 0 then
+        TempResult := TempResult + Expr[c];
+    end;
+
+    if ContainsValidOR(TempResult) then
+      Result := 'OR'
+    else
+      Result := ''; // Não encontrou "OR" válido fora dos parênteses
+  end;
+
+var
+  Conditions   : TStringList;
+  Condition    : String;
+  SQLSanitized : String;
+begin
+  Result := False;
+  SQLSanitized := LinearizeSQL(ASQL);
+  Conditions := ExtractWhereConditionsWithOR(ASQL);
+  try
+    for Condition in Conditions do
+    begin
+      Result := not ExtractInternalOperators(Condition).IsEmpty;
+      if Result then
+        Break;
+    end;
+  finally
+    Conditions.Free;
+  end;
 end;
 
 
