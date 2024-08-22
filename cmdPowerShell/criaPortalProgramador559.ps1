6dcd21b702b383637278c668be57c7e6cb0de0c0
flsantos <flsantos@apdata.com.br>
2024-08-22 16:43:15 -0300

    Commit realizado em 2024-08-22 16:43:15

--- cmdPowerShell/criaPortalProgramador559.ps1
+++ cmdPowerShell/criaPortalProgramador559.ps1
@@ -1,4 +1,3 @@
-# Verificar se o script está sendo executado como administrador
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
@@ -6,29 +5,33 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 
 Import-Module WebAdministration
 
-# Nome fixo do site e do aplicativo
 $siteName = "559"
 $appName = ".net"
 
-# Verificar se o diretório virtual já existe dentro do "Default Web Site"
 $virtualDirPath = "IIS:\Sites\Default Web Site\$siteName"
 $virtualDirExists = Test-Path $virtualDirPath
 
 if ($virtualDirExists) {
     $userResponse = Read-Host "O diretorio virtual $siteName ja existe dentro do 'Default Web Site'. Deseja deletar e criar um novo? (Sim/Nao)"
     if ($userResponse -eq "Nao") {
-        Write-Host "Operacao cancelada pelo usuario."
-        exit
+        $siteName = Read-Host "Digite um novo nome para o site"
+        $virtualDirPath = "IIS:\Sites\Default Web Site\$siteName"
+        $virtualDirExists = Test-Path $virtualDirPath
+        
+        while ($virtualDirExists) {
+            Write-Host "O nome $siteName já existe. Digite um nome diferente."
+            $siteName = Read-Host "Digite um novo nome para o site"
+            $virtualDirPath = "IIS:\Sites\Default Web Site\$siteName"
+            $virtualDirExists = Test-Path $virtualDirPath
+        }
     } else {
         Remove-Item $virtualDirPath -Recurse -Force
         Write-Host "Diretorio virtual $siteName deletado."
     }
 }
 
-# Solicitar informações do usuário
 $basePhysicalPath = Read-Host "Digite a base do caminho fisico (por exemplo, C:\Apdata_X64)"
 
-# Complementar os caminhos
 $physicalPath = Join-Path $basePhysicalPath "\Aplicacoes\ApWebDispatcher\Site"
 $appPhysicalPath = Join-Path $basePhysicalPath "\Aplicacoes\ApWebDispatcher\ApWebDispatcher"
 
@@ -37,45 +40,34 @@ $username = Read-Host "Digite o nome de usuario para conectar"
 $password = Read-Host "Digite a senha"
 
 if (-not $bindings) {
-    # Criar ou atualizar o diretório virtual
     New-Item "IIS:\Sites\Default Web Site\$siteName" -type VirtualDirectory -physicalPath $physicalPath -Force
 
-    # Configurar a identidade do diretório virtual
     Set-WebConfigurationProperty -filter "/system.applicationHost/sites/site[@name='Default Web Site']/virtualDirectory[@path='/`$siteName']" -name "userName" -value $username
     Set-WebConfigurationProperty -filter "/system.applicationHost/sites/site[@name='Default Web Site']/virtualDirectory[@path='/`$siteName']" -name "password" -value $password
 	
-	# Configurando as credenciais do caminho físico para o diretório virtual
 	$virtualDirConfigPath = "/system.applicationHost/sites/site[@name='Default Web Site']/virtualDirectory[@path='/`$siteName']"
 	Set-WebConfiguration -filter "$virtualDirConfigPath/@userName" -value $username
 	Set-WebConfiguration -filter "$virtualDirConfigPath/@password" -value $password	
 	
-	# Caminho para o diretório virtual no provedor WebAdministration
 	$virtualDirPath = "IIS:\Sites\Default Web Site\$siteName"
 
-	# Definindo as credenciais do caminho físico para o diretório virtual
 	Set-ItemProperty -Path $virtualDirPath -Name "userName" -Value $username
 	Set-ItemProperty -Path $virtualDirPath -Name "password" -Value $password	
 	
-    # Criar ou atualizar o aplicativo
     New-Item "IIS:\Sites\Default Web Site\$siteName\$appName" -type Application -physicalPath $appPhysicalPath -Force
 
-    # Configurar a identidade do aplicativo
     Set-WebConfigurationProperty -filter "/system.applicationHost/sites/site[@name='Default Web Site']/application[@path='/`$siteName/`$appName']" -name "userName" -value $username
     Set-WebConfigurationProperty -filter "/system.applicationHost/sites/site[@name='Default Web Site']/application[@path='/`$siteName/`$appName']" -name "password" -value $password
 
-	# Configurando as credenciais do caminho físico para o aplicativo
 	$virtualDirConfigPathApp = "/system.applicationHost/sites/site[@name='Default Web Site']/application[@path='/`$siteName/`$appName']/virtualDirectory[@path='/']"
 	Set-WebConfiguration -filter $virtualDirConfigPathApp -value @{userName=$username; password=$password}
 
 } else {
-    # Criar ou atualizar o site
     New-Item "IIS:\Sites\$siteName" -physicalPath $physicalPath -bindings $bindings -Force
 
-    # Configurar a identidade do site
     Set-WebConfigurationProperty -filter "/system.applicationHost/sites/site[@name=`"$siteName`"]" -name "userName" -value $username
     Set-WebConfigurationProperty -filter "/system.applicationHost/sites/site[@name=`"$siteName`"]" -name "password" -value $password
 
-    # Criar ou atualizar o aplicativo dentro do site
     New-Item "IIS:\Sites\$siteName\$appName" -type Application -physicalPath $appPhysicalPath -Force
 }
 
