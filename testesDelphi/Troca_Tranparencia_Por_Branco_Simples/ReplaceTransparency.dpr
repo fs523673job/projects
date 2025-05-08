49098215cb32c348a921fb8f3b9ca66487db80cf
flsantos <flsantos@apdata.com.br>
2025-05-08 11:04:09 -0300

    Commit realizado em 2025-05-08 11:04:09

--- testesDelphi/Troca_Tranparencia_Por_Branco_Simples/ReplaceTransparency.dpr
+++ testesDelphi/Troca_Tranparencia_Por_Branco_Simples/ReplaceTransparency.dpr
@@ -5,6 +5,7 @@ program ReplaceTransparency;
 uses
   SysUtils,
   Windows,
+  Types,
   Vcl.Graphics,
   Vcl.Imaging.pngimage;
 
@@ -73,6 +74,84 @@ begin
   end;
 end;
 
+procedure RemoverTransparenciaCorretamente(APng: TPngImage; CorFundo: TColor);
+var
+  X, Y: Integer;
+  AlphaLine: PByteArray;
+  RGBLine: PRGBLine;
+  FundoR, FundoG, FundoB: Byte;
+  Alfa: Byte;
+  AlfaNormalizado: Single;
+begin
+  // Componentes RGB da cor de fundo
+  FundoR := GetRValue(ColorToRGB(CorFundo));
+  FundoG := GetGValue(ColorToRGB(CorFundo));
+  FundoB := GetBValue(ColorToRGB(CorFundo));
+
+  APng.Canvas.Lock;
+  try
+    for Y := 0 to APng.Height - 1 do
+    begin
+      AlphaLine := APng.AlphaScanline[Y];
+      RGBLine := APng.Scanline[Y];
+
+      if Assigned(AlphaLine) then
+      begin
+        for X := 0 to APng.Width - 1 do
+        begin
+          Alfa := AlphaLine^[X];
+
+          // Se houver transparência parcial
+          if Alfa < 255 then
+          begin
+            AlfaNormalizado := Alfa / 255;
+
+            // Composição correta sobre o fundo
+            RGBLine^[X].rgbtRed := Round((RGBLine^[X].rgbtRed * AlfaNormalizado) + (FundoR * (1 - AlfaNormalizado)));
+            RGBLine^[X].rgbtGreen := Round((RGBLine^[X].rgbtGreen * AlfaNormalizado) + (FundoG * (1 - AlfaNormalizado)));
+            RGBLine^[X].rgbtBlue := Round((RGBLine^[X].rgbtBlue * AlfaNormalizado) + (FundoB * (1 - AlfaNormalizado)));
+
+            // Remove a transparência totalmente
+            AlphaLine^[X] := 255;
+          end;
+        end;
+      end;
+    end;
+
+    // Remove a informação de canal alfa da imagem
+    APng.RemoveTransparency;
+  finally
+    APng.Canvas.Unlock;
+  end;
+end;
+
+procedure RemoverTransparenciaPNG(APng: TPngImage; const ArquivoEntrada, ArquivoSaida: String; CorFundo: TColor);
+var
+  Bitmap: TBitmap;
+  RectImg: TRect;
+begin
+  if not FileExists(ArquivoEntrada) then
+    raise Exception.Create('Arquivo não encontrado.');
+
+  Bitmap := TBitmap.Create;
+  try
+    Bitmap.PixelFormat := pf32bit;
+    Bitmap.AlphaFormat := afIgnored; // Sem alfa
+    Bitmap.SetSize(APng.Width, APng.Height);
+
+    Bitmap.Canvas.Brush.Color := CorFundo;
+    Bitmap.Canvas.FillRect(Rect(0, 0, Bitmap.Width, Bitmap.Height));
+
+    RectImg := Rect(0, 0, APng.Width, APng.Height);
+    APng.Draw(Bitmap.Canvas, RectImg);
+
+    APng.Assign(Bitmap);
+    APng.RemoveTransparency;
+  finally
+    Bitmap.Free;
+  end;
+end;
+
 var
   Png: TPngImage;
   ArquivoEntrada, ArquivoSaida: String;
@@ -92,9 +171,12 @@ begin
       begin
         Writeln('Imagem possui transparência. Removendo...');
 
-        RemoverTransparencia(Png, clWhite);
-
         ArquivoSaida := ChangeFileExt(ArquivoEntrada, '_SemTransparencia.png');
+
+        RemoverTransparenciaCorretamente(Png, clBlack);
+        RemoverTransparencia(Png, clBlack);
+        RemoverTransparenciaPNG(Png, ArquivoEntrada, ArquivoSaida, clBlack);
+
         Png.SaveToFile(ArquivoSaida);
 
         Writeln('Transparência removida e salvo em: ', ArquivoSaida);
