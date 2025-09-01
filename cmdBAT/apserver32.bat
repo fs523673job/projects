94bfc8ef423c1143e339f3ce9f743f15a5eb72cb
flsantos <flsantos@apdata.com.br>
2025-09-01 09:44:25 -0300

    Commit realizado em 2025-09-01  9:44:24

--- cmdBAT/apserver32.bat
+++ cmdBAT/apserver32.bat
@@ -8,6 +8,23 @@ set "ARQ_DEFAULT=Win32"
 set "BLD_DEFAULT=Debug"
 set "PRN_DEFAULT=localhost /start /enablelog /UseHTTPS /integration_timeout 130"
 
+if not "%~2"=="" (
+    set "VRS_DEFAULT=%~1"
+    set "BLD_DEFAULT=%~2"
+) else (
+    if not "%~1"=="" (
+        if /I "%~1"=="Debug" (
+            set "BLD_DEFAULT=Debug"
+        ) else if /I "%~1"=="Release" (
+            set "BLD_DEFAULT=Release"
+        ) else if /I "%~1"=="Memleak" (
+            set "BLD_DEFAULT=Memleak"
+        ) else (
+            set "VRS_DEFAULT=%~1"
+        )
+    )
+)
+
 set "PROJECT_ROOT=%~dp0.."
 
 :: Chama o primeiro script com os valores padrão
